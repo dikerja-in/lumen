015287f4b686a00514b417560ca96d61f1fe3899
Triadi Arifin <triadiar0826@gmail.com>
2020-10-23 05:57:54 +0700

    Return json for abort(404) and abort(405)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -49,6 +51,20 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'code' => 0,
+                'error' => 'Route not found.',
+            ], 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'code' => 0,
+                'error' => 'Method not allowed.',
+            ], 405);
+        }
+
         return parent::render($request, $exception);
     }
 }
